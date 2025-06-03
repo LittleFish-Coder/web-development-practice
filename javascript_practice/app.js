c3af39a61e50df75c455b22e0770ece4f2f7c646
LittleFish-Coder <adrianyu890317@gmail.com>
2025-06-03 21:03:22 +0800

    add javascript practice

---
+++ javascript_practice/app.js
@@ -0,0 +1,24 @@
+let x = 10;
+console.log(x);
+
+// In convention constant variables are written in uppercase
+const PI = 3.14;
+console.log(PI);
+
+// Truthy
+if (x) {
+    console.log("x is truthy");
+} else {
+    console.log("x is falsy");
+}
+
+// Falsy values in JavaScript
+// 0, "", null, undefined, NaN, empty arrays and objects
+
+// Falsy
+if (0) {
+    console.log("0 is truthy");
+}
+else {
+    console.log("0 is falsy");
+}
